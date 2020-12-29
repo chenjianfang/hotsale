70959cb4d7a3b9c380d8a93dfdc14efb393f595c
chenjianfang <jianfang.chen@zhenai.com>
2020-12-29 16:58:19 +0800

    修改接口日志

--- server/router/spider.js
+++ server/router/spider.js
@@ -23,33 +23,37 @@ router.get('/spider', async (ctx) => {
         console.log({ keyword, sort, style });
 
         const url = `https://list.tmall.com/search_product.htm?q=${encodeURIComponent(keyword)}&sort=${sort}&style=${style}`;
-        const res = await get(url);
-        const $ = cheerio.load(res);
-        const data = $('#J_ItemList').find('.product');
-        Array.from(data).forEach((item) => {
-            const obj = {};
-            const productImg = $(item).find('.productImg-wrap');
-            const productNum = $(item).find('.productStatus').children('span');
-            let sales = $(productNum.get(0)).text().match(/[1-9.]+/g);
-            if (sales) {
-                sales = sales[0].includes('.') ? `${sales[0]}万笔` : sales[0];
-            } else {
-                sales = 0;
-            }
-            const imgNode = $($(productImg).find('img')[0]);
-            obj.link = $(productImg).find('a').attr('href');
-            obj.img = imgNode.attr('data-ks-lazyload') || imgNode.attr('src');
-            obj.price = $(item).find('.productPrice').text().replace(/\n/g, '');
-            obj.sales = sales;
-            result.push(obj);
-        });
+        try {
+            const res = await get(url);
+            const $ = cheerio.load(res);
+            const data = $('#J_ItemList').find('.product');
+            Array.from(data).forEach((item) => {
+                const obj = {};
+                const productImg = $(item).find('.productImg-wrap');
+                const productNum = $(item).find('.productStatus').children('span');
+                let sales = $(productNum.get(0)).text().match(/[1-9.]+/g);
+                if (sales) {
+                    sales = sales[0].includes('.') ? `${sales[0]}万笔` : sales[0];
+                } else {
+                    sales = 0;
+                }
+                const imgNode = $($(productImg).find('img')[0]);
+                obj.link = $(productImg).find('a').attr('href');
+                obj.img = imgNode.attr('data-ks-lazyload') || imgNode.attr('src');
+                obj.price = $(item).find('.productPrice').text().replace(/\n/g, '');
+                obj.sales = sales;
+                result.push(obj);
+            });
 
-        db.insertDocuments([
-            {
-                key: getDBKey({ keyword, sort, style }),
-                value: result
-            }
-        ]);
+            db.insertDocuments([
+                {
+                    key: getDBKey({ keyword, sort, style }),
+                    value: result
+                }
+            ]);
+        } catch (e) {
+            console.log(e);
+        }
     } else {
         result = docs.pop().value;
     }
